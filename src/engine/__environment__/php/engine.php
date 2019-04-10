68917375ab2f81814117f33c5943817c6d6d80e4
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-10 09:20:51 -0400

    Updates debugging to check both `HTTP_HOST` and `SERVER_NAME`

--- src/engine/__environment__/php/engine.php
+++ src/engine/__environment__/php/engine.php
@@ -4,7 +4,7 @@
 require "init.php";
 
 // Enable debugging during development.
-if( $_SERVER['SERVER_NAME'] == 'localhost' ) {
+if( $_SERVER['HTTP_HOST'] == 'localhost' or $_SERVER['SERVER_NAME'] == 'localhost' ) {
   
   // Report all errors.
   error_reporting(E_ALL);
