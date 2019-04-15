67c3c95a1d80dba27b6341fbed7ecd3a0339694d
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-15 09:31:01 -0400

    Adds absolute path helpers

--- src/engine/__environment__/php/libraries/path.php
+++ src/engine/__environment__/php/libraries/path.php
@@ -7,4 +7,38 @@ function cleanpath( $unclean ) {
   
 }
 
+// Converts a relative path to an absolute path.
+function absolute_path( $path ) {
+  
+  // Normalize directory separators.
+  $path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+  
+  // Get absolute path parts.
+  $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+  
+  // Initialize the absolute path.
+  $absolute = [];
+  
+  // Convert relative path parts into absolute path parts.
+  foreach( $parts as $part ) {
+    
+    // Recognize `.` as the current directory.
+    if( $path == '.' ) continue;
+    
+    // Recognize `..` as the previous directory.
+    if( $path == '..' ) array_pop($absolute);
+    
+    // Otherwise, capture the path part.
+    else $absolute[] = $part;
+    
+  }
+  
+  // Return the absolute path.
+  return implode(DIRECTORY_SEPARATOR, $absolute);
+  
+}
+
+// Converts a relative path to an absolute, root-relative path.
+function absolute_path_from_root( $path ) { return cleanpath('/'.absolute_path($path)); }
+
 ?>
