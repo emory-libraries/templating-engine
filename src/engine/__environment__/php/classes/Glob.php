1f8218256dc4b43bb5d648e926ad1c30e00a7b80
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-17 12:54:54 -0400

    Starts to create Glob class

--- src/engine/__environment__/php/classes/Glob.php
+++ src/engine/__environment__/php/classes/Glob.php
@@ -7,13 +7,38 @@
  */
 class Glob {
   
-  // Filters on values.
+  // Filters by values.
   const FILTER_VALUE = 1;
   
-  // Filters on keys.
+  // Filters by keys.
   const FILTER_KEY = 2;
   
   // TODO: Create globbing library for `MatchHelpers`. Refer to [webmozart/glob](https://github.com/webmozart/glob/).
+  
+  // Expands a glob pattern into an array of paths.
+  public static function glob( $pattern ) {
+    
+    
+    
+  }
+  
+  // Determines if a path matches a glob pattern.
+  public static function match( $path, $pattern ) {
+    
+    
+  }
+  
+  // Filters an array of paths for only paths that match a given glob pattern.
+  public static function filter( array $paths, $pattern, $flags == self::FILTER_VALUE ) {
+    
+  }
+  
+  // Determines if a glob pattern is dynamic.
+  public static function isDynamic( $glob ) {
+    
+    return (strpos($glob '*') !== false or strpos($glob, '{') !== false or strpos($glob, '?') !== false or strpos($glob, '[') !== false); 
+    
+  }
   
 }
 
