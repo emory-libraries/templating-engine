929e784059d2dfd19fa0595ff614ea8bba7254bb
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-18 09:16:39 -0400

    Updates the templating engine to enable error logging and debugging in our remote development environment

--- src/engine/__environment__/php/engine.php
+++ src/engine/__environment__/php/engine.php
@@ -4,7 +4,7 @@
 require "init.php";
 
 // Enable debugging during development.
-if( $_SERVER['HTTP_HOST'] == 'localhost' or $_SERVER['SERVER_NAME'] == 'localhost' ) {
+if( $_SERVER['HTTP_HOST'] == 'localhost' or $_SERVER['SERVER_NAME'] == 'localhost' or ENVIRONMENT == 'development' ) {
   
   // Report all errors.
   error_reporting(E_ALL);
