b0bb84348e8a64797093ec31aa57143b384be56c
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-18 11:03:34 -0400

    Updates class autoloader to enable subdirectories

--- src/engine/__environment__/php/classes/autoload.php
+++ src/engine/__environment__/php/classes/autoload.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function($class) {
   
   // Parse the file path.
-  $path = __DIR__."/$class.php";
+  $path = __DIR__.'/'.str_replace("\\", DIRECTORY_SEPARATOR, $class).'.php';
   
   // Verify the file.
   if( file_exists($path) ) include $path;
