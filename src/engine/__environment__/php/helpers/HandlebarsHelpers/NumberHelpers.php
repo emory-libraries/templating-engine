3ecd117035a6313a3f653fc26849c3c919476f94
Lauren Hamel <lauren.hamel@outlook.com>
2019-05-17 16:54:12 -0400

    Adds number handlebars helpers

--- src/engine/__environment__/php/helpers/HandlebarsHelpers/NumberHelpers.php
+++ src/engine/__environment__/php/helpers/HandlebarsHelpers/NumberHelpers.php
@@ -28,7 +28,7 @@ trait NumberHelpers {
     // Get the index of the last item in the byte abbreviations.
     $index = count($abbr) - 1;
     
-    // Determine if appropriate abbreviation for the number.
+    // Determine the appropriate abbreviation for the number.
     while( $index-- >= 0 ) {
       
       // Get the bytes size as a power of 10.
@@ -66,19 +66,164 @@ trait NumberHelpers {
     
   }
   
-  // TODO: Create definition for `phoneNumber` helper.
+  // Convert a string or number to a formatted phone number.
+  public static function phoneNumber( $number ) {
+    
+    // Convert the number to a string.
+    $number = (string) $number;
+    
+    // Get the phone number's parts.
+    $area = substr($number, 0, 3);
+    $prefix = substr($number, 3, 3);
+    $line = substr($number, 6, 4);
+    
+    // Format the number as a phone number.
+    return "($area) $prefix-$line";
+    
+  }
   
-  // TODO: Create definition for `toAbbr` helper.
+  // Abbreviate numbers to the given `precision` number of decimal points.
+  public static function toAbbr( $number = 0, $precision = 2 ) {
+    
+    // Set the defaults for number and precision.
+    $precision = func_num_args() > 2 ? $precision : 2;
+    $number = func_num_args() > 1 ? $number : 0;
+    
+    // Get precision as a power of 10.
+    $precision = pow(10, $precision);
+    
+    // Set number abbreviations.
+    $abbr = ['k', 'm', 'b', 't', 'q'];
+    
+    // Get the index of the last item in the number abbreviations.
+    $index = count($abbr) - 1;
+    
+    // Determine the appropriate abbreviation for the number.
+    while( $index >= 0 ) {
+      
+      // Get the number size as a power of 10.
+      $size = pow(10, ($index + 1) * 3);
+      
+      // Determine if the number size matches the number.
+      if( $size <= ($number + 1) ) {
+        
+        // Get the number with the correct precision.
+        $number = round($number * $precision / $size) / $precision;
+        
+        // Capture the abbreviation.
+        $number .= $abbr[$index];
+        
+        // Stop looking for the correct abbreviation.
+        break;
+        
+      }
+      
+      // Decrement the index.
+      $index--;
+      
+    }
+    
+    // Return the abbreviated number.
+    return $number;
+    
+  }
   
-  // TODO: Create definition for `toExponential` helper.
+  // Return the string representing the given number in exponential notation.
+  public static function toExponential( $number = 0, $digits = 0 ) {
+    
+    // Set the defaults for number and digits.
+    $digits = func_num_args() > 2 ? $digits : 0;
+    $number = func_num_args() > 1 ? $number : 0;
+    
+    // Get the remainder of the number when divided by 10, and its base number divisible by 10.
+    $remainder = fmod($number, 10);
+    $base = $number - $remainder;
+    
+    // Determine if the base or remainder should be used in determining places.
+    $n = $base === 0 ? $remainder : $base;
+    
+    // Initialize a counter for capturing places.
+    $places = 0;
+    
+    // Get the digit that should be used as the stopping point when looking for places.
+    $digit = (int) ((string) $number)[0];
+    
+    // Capture the number of times the number can be divided by 10.
+    while( $n <> $digit ) {
+      
+      // Increment or decrement places.
+      $places = $n < 1 ? $places - 1 : $places + 1;
+      
+      // Reset n, and continue counting places.
+      $n = $n < 1 ? $n * 10 : $n / 10;
+      
+    }
+    
+    // Capture the number's base 10 power.
+    $power = pow(10, $places);
+    
+    // Return the number formated using exponential notation.
+    return (number_format($number / $power, $digits, '.', '') . 'e'. ($places > 0 ? '+'. $places : $places));
+    
+  }
   
-  // TODO: Create definition for `toFixed` helper.
+  // Formats the given number using fixed-point notation.
+  public static function toFixed( $number = 0, $digits = 0 ) {
+    
+    // Set the defaults for number and digits.
+    $digits = func_num_args() > 2 ? $digits : 0;
+    $number = func_num_args() > 1 ? $number : 0;
+    
+    // Format the number using fixed-point notation.
+    return number_format($number, $digits, '.', '');
+    
+  }
   
-  // TODO: Create definition for `toFloat` helper.
+  // Cast a number to a floating point value.
+  public static function toFloat( $number ) { return (float) $number; }
   
-  // TODO: Create definition for `toInt` helper.
+  // Cast a number to an integer value.
+  public static function toInt( $number ) { return (int) $number; }
   
-  // TODO: Create definition for `toPrecision` helper.
+  // Formats the given number to the specified precision.
+  public static function toPrecision( $number = 0, $precision = 1 ) {
+    
+    // Set the defaults for number and precision.
+    $precision = func_num_args() > 2 ? $precision : 1;
+    $number = func_num_args() > 1 ? $number : 0;
+    
+    // Always return 0 as is.
+    if( $number === 0 ) return 0;
+    
+    // Get the digits before and after the decimal place.
+    $digits = explode('.', (string) $number);
+    
+    // Get the number of digits to the left and right of the decimal place.
+    $length = array_map('strlen', $digits);
+    
+    // If precision < number of digits before the decimal, then use exponential notation.
+    if( $precision < $length[0] ) return forward_static_call('HandlebarsHelpers\NumberHelpers::toExponential', $number, $precision - 1);
+    
+    // Otherwise, find significant digits.
+    $power = floor(log(abs($number), 10) + 1);
+    $significant = round($number / pow(10, $power) * pow(10, $precision)) / pow(10, $precision);
+    
+    // Capture the significant value.
+    $value = $significant * pow(10, $power);
+    
+    // Get the value's length.
+    $length = strlen((string) $value);
+    
+    // If precision < number of digits in the value, the convert it to exponential notation.
+    if( $precision < $length ) return forward_static_call('HandlebarsHelpers\NumberHelpers::toExponential', $value, $precision - 1);
+    
+    // Otherwise, if precision > number of digits in value, then fill the number with 0 digits.
+    if( $precision > $length ) return $value.str_repeat('0', $precision - $length);
+    
+    // Otherwise, return the value as is.
+    return $value;
+    
+  }
   
 }
 
