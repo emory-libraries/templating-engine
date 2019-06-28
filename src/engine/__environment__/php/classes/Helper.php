c3cf20be26451b1dc32f1ce846306067f21908d5
Lauren Hamel <lauren.hamel@outlook.com>
2019-06-28 17:07:29 -0400

    Updates patterns and assets

--- src/engine/__environment__/php/classes/Helper.php
+++ src/engine/__environment__/php/classes/Helper.php
@@ -7,32 +7,40 @@
  * another. This is mostly needed for layout helpers.
  */
 class Helper {
-  
+
   // Store some data temporarily.
   public static $data = [];
-  
+
   // Set some data temporarily.
   public static function set( string $helper, string $key, $value ) {
-    
+
     // Temporarily store the data.
     self::$data = array_set(self::$data, "$helper.$key", $value);
-    
+
   }
-  
+
   // Get some temporary data, then unset it.
   public static function get( string $helper, string $key, $default = null ) {
-    
+
     // Get the value that was stored temporarily.
     $value = array_get(self::$data, "$helper.$key", $default);
-    
+
     // Unset the temporary value.
     self::$data = array_unset(self::$data, "$helper.$key");
-    
+
     // Return the value that was stored temporarily.
     return $value;
+
+  }
+
+  // Determine if some temporary data exists.
+  public static function has( string $helper, string $key ) {
+
+    // Look for the key within the temporary data set.
+    return array_has(self::$data, $key);
     
   }
-  
+
 }
 
 ?>
--- src/engine/__environment__/php/helpers/CustomHelpers/StorageHelpers.php
+++ src/engine/__environment__/php/helpers/CustomHelpers/StorageHelpers.php
@@ -7,49 +7,56 @@ namespace CustomHelpers;
  * NOTE: These `StorageHelpers` are needed due to known limitations within LightnCandy's implementation of custom helpers. Because LightnCandy does not properly carry changes to child contexts upward into parent contexts, these `StorageHelpers` are a workaround to allow for context data to stored temporarly, then retrieved and reapplied on parent contexts. See issue [#268](https://github.com/zordius/lightncandy/issues/268) for more details.
  */
 trait StorageHelpers {
-  
+
   // Temporarily store a value in storage.
   public static function storageSet( $key, $value ) {
-    
+
     Helper::set('CustomHelpers\VarHelpers::storage', $key, $value);
-    
+
   }
-  
+
   // Push a value onto the end of an temporary array in storage.
   public static function storagePush( $key, $value ) {
-    
+
     // Get the array from storage.
     $array = Helper::get('CustomHelpers\VarHelpers::storage', $key, []);
-    
+
     // Push the value to the array.
     array_push($array, $value);
 
     // Restore the array in storage.
     Helper::set('CustomHelpers\VarHelpers::storage', $key, $array);
-    
+
   }
-  
+
   // Push a value onto the beginning of an temporary array in storage.
   public static function storageUnshift( $key, $value ) {
-    
+
     // Get the array from storage.
     $array = Helper::get('CustomHelpers\VarHelpers::storage', $key, []);
-    
+
     // Push the value to the array.
     array_unshift($array, $value);
 
     // Restore the array in storage.
     Helper::set('CustomHelpers\VarHelpers::storage', $key, $array);
-    
+
   }
-  
+
   // Get a value temporarily stored in storage.
   public static function storageGet( $key ) {
-    
+
     return Helper::get('CustomHelpers\VarHelpers::storage', $key);
-    
+
   }
-  
+
+  // Determine if a value is set within storage.
+  public static function storageHas( $key ) {
+
+    return Helper::has('CustomHelpers\VarHelpers::storage', $key);
+
+  }
+
 }
 
 ?>
