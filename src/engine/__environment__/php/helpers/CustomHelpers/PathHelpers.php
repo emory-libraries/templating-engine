5613936d87d13f256312c8ede1601d5e18a467fc
Lauren Hamel <lauren.hamel@outlook.com>
2019-07-19 11:42:36 -0400

    Fixes hero images not showing on exhibition pages by adding additional path helpers and forcing the background image path to be absolute

---
+++ src/engine/__environment__/php/helpers/CustomHelpers/PathHelpers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CustomHelpers;
+
+trait PathHelpers {
+
+  // Get the base URL for the given site.
+  public static function baseUrl( ) {
+
+    // Determine the base path based on whether development mode is enabled.
+    //$base = (CONFIG['localhost'] or CONFIG['ngrok']) ? '/'.str_replace(CONFIG['document']['root'], '', CONFIG['site']['root']) : '';
+
+    $base = (CONFIG['localhost'] or CONFIG['ngrok']) ? ''.str_replace(CONFIG['document']['root'], '', CONFIG['site']['root']) : '';
+
+    // Return the site's base URL.
+    return cleanpath($base);
+
+  }
+
+  // Convert a path into an absolute path.
+  public static function absolutePath( string $path ) {
+
+    // Convert the path to an absolute path.
+    return Path::toAbsolute($path);
+
+  }
+
+  // Convert a path into a relative path.
+  public static function relativePath( string $path ) {
+
+    // Convert the path to a relative path.
+    return Path::toRelative($path);
+
+  }
+
+  // Convert a path into a root relative path.
+  public static function rootRelativePath( string $path ) {
+
+    // Convert the path to a root relative path.
+    return Path::toRootRelative($path);
+
+  }
+
+}
+
+?>
